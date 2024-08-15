0706fdf33be173339ccbeb6b28b62f91eb379e66
nivas <nivasm31@gmail.com>
2024-08-15 21:01:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+// App..js
+
+import "./App.css";
+import { useState } from "react";
+import ReactPlayer from "react-player";
+import movieTrailer from "movie-trailer";
+
+function App() {
+  //Setting up the initial states using
+  // react hook 'useState"
+  const [video, setVideo] = useState("inception");
+  const [videoURL, setVideoURL] = useState("https://youtu.be/YoHD9XEInc0");
+
+  //A function to fetch the required URL
+  // and storing it inside the
+  // videoURL state variable
+  function handleSearch() {
+    movieTrailer(video).then((res) => {
+      setVideoURL(res);
+    });
+  }
+
+  return (
+    <div className="App">
+      <div className="search-box">
+        <label>Search for any movies/shows: </label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setVideo(e.target.value);
+          }}
+        />
+
+        <button
+          onClick={() => {
+            handleSearch();
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <ReactPlayer url={videoURL} controls={true} />
+    </div>
+  );
+}
+
+export default App;
